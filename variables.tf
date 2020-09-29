8dbfc335e4898cf5aac5901f130355126e2e0187
femog008 <femog008@yahoo.co.uk>
2020-09-29 12:24:06 -0400

    Removed default values

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources job"
-  default = "uda-proja"
 }
 
 variable "location" {
@@ -10,20 +9,16 @@ variable "location" {
 
 variable "username" {
   description = "The Admin account to login to the server."
-  default = "femog008"
 }
 
 variable "password" {
   description = "The password of the admin account to be created."
-  default = "P@ssw0rd#456"
 }
 
 variable "environment_tag" {
   description = "The tag to be added for the environment-tag on the resource to be created. Example 'Dev'"
-  default = "dev"
 }
 
 variable "number_of_vm" {
   description = "The number of virtual machines to be provisioned"
-  default = "3"
 }
